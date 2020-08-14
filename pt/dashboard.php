087da2cef56df529a7a9add99173f0c9a824c132
Anny <annyufrr@gmail.com>
2020-08-14 16:54:31 -0300

    post diretory

---
+++ pt/dashboard.php
@@ -0,0 +1,11 @@
+<?php
+return array(
+    'dashboard' => 'Dashboard',
+    'chart' => 'Chart',
+    'nothing_to_show' => 'All clear, nothing to show!',
+    'stats' => array(
+        'total' => 'Total :attribute',
+        'count' => ':attribute Count',
+        'today' => ':attribute Today',
+    ),
+);
